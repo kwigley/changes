de6cda1b30426246cd476b73ec1c06660f2312fe
Kyle Wigley <kwigley44@gmail.com>
2022-03-01 17:47:11 -0500

    use `IntoIterator`

--- src/ui.rs
+++ src/ui.rs
@@ -6,13 +6,29 @@ use crate::error::Result;
 pub fn select_input<T, I>(items: I) -> Result<Option<T>>
 where
     T: ToString + Copy,
-    I: Iterator<Item = T> + Clone,
+    I: IntoIterator<Item = T> + Clone,
 {
-    Ok(items.clone().nth(
+    Ok(items.clone().into_iter().nth(
         Select::with_theme(&ColorfulTheme::default())
             .with_prompt("What type of change is this?")
             .default(0)
-            .items(&items.map(|v| v.to_string()).collect::<Vec<String>>())
+            .items(
+                &items
+                    .into_iter()
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>(),
+            )
             .interact()?,
     ))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_select_input() -> Result<()> {
+        select_input(vec!["option1", "option2"])?;
+        Ok(())
+    }
 }
